e0b017ede0899e23ad433b098b81ecf7bd76ac42
Ben Pearson <benpearson61@googlemail.com>
2024-12-21 12:15:38 +0900

    refactored

--- config/routes.rb
+++ config/routes.rb
@@ -7,15 +7,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get "tasks", to: "tasks#index", as: "tasks"
+  Rails.application.routes.draw do
+    resources :tasks
+  end
+  # get "tasks", to: "tasks#index", as: "tasks"
 
-  get "tasks/new", to: "tasks#new"
-  post "tasks", to: "tasks#create"
+  # get "tasks/new", to: "tasks#new"
+  # post "tasks", to: "tasks#create"
 
-  get "tasks/:id/edit", to: "tasks#edit"
-  patch "tasks/:id", to: "tasks#update"
+  # get "tasks/:id/edit", to: "tasks#edit"
+  # patch "tasks/:id", to: "tasks#update"
 
-  get "tasks/:id", to: "tasks#show", as: "task"
+  # delete "tasks/:id", to: "tasks#destroy"
 
-
+  # get "tasks/:id", to: "tasks#show", as: "task"
 end
